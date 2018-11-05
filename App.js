fe4bdef8777c4fee44cb00d93a9e5d06a870f098
vitaminwater <stantin04@gmail.com>
2018-11-05 12:32:48 +0100

    upgraded everything

--- App.js
+++ App.js
@@ -4,9 +4,6 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
-
 import AppReducer from './src/reducers'
 import AppWithNavigationState from './src/navigators/AppNavigator'
 import { rootSaga } from './src/sagas';
@@ -20,7 +17,6 @@ const composeEnhancers =
     // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
   }) : compose;
 const store = createStore(AppReducer, composeEnhancers(applyMiddleware(sagaMiddleware)))
-const persistor = persistStore(store)
 
 sagaMiddleware.run(rootSaga)
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,5 @@
 import { fromJS } from 'immutable';
-import { persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
 import { combineReducers } from 'redux-immutable'
-import immutableTransform from 'redux-persist-transform-immutable'
-
-export const persistConfig = {
-  transforms: [immutableTransform()],
-  key: 'persist',
-  storage,
-}
 
 import ble from './ble'
 
